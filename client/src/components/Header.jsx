03fc5fa19a8204b2bf7441756eb0d5b55026091e
Ketan Khot <deltacodingketan@gmail.com>
2024-07-18 12:42:01 +0530

    Header Component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-slate-200 p-4">
+      <div className="flex justify-between max-w-6xl items-center mx-auto">
+        <Link to="/">
+          <h1 className="font-bold text-2xl">Auth App</h1>
+        </Link>
+
+        <ul className="flex gap-5 font-medium text-xl">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li>Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
