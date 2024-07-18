a32c7f67aa68b283b819d8fff24c7f1afdd518c4
Ketan Khot <deltacodingketan@gmail.com>
2024-07-18 15:44:04 +0530

    Create User model

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// Creating the Mongoose Database Schema for the user
+// Schema is used for the structure of the information store in DB
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// In databases, particularly in MongoDB, the timestamps: true option is used to automatically add and manage createdAt and updatedAt fields to your schema. These fields store the timestamps for when a document was created and last updated, respectively.
+
+// Creating the Mongoose model for the user schema
+const User = mongoose.model("User", userSchema);
+
+// Export
+export default User;
